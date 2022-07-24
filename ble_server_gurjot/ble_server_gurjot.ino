dcf3d7959ec0fc789df2d38dd76a4c33dd03c216
Rishabh Bokadiya <rishabhbokadiya2003@gmail.com>
2022-07-24 13:29:58 +0530

    trying to add reconnect

--- ble_server_gurjot/ble_server_gurjot.ino
+++ ble_server_gurjot/ble_server_gurjot.ino
@@ -14,47 +14,50 @@
 
 BLECharacteristic *pCharacteristic;
 
-#define SERVICE_UUID        "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+#define SERVICE_UUID "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
 BLECharacteristic soilCharacteristics("f78ebbff-c8b7-4107-93de-889a6a06d408", BLECharacteristic::PROPERTY_NOTIFY);
 BLEDescriptor soilDescriptor(BLEUUID((uint16_t)0x2901));
 BLECharacteristic smokeCharacteristics("ca73b3ba-39f6-4ab3-91ae-186dc9577d99", BLECharacteristic::PROPERTY_NOTIFY);
 BLEDescriptor smokeDescriptor(BLEUUID((uint16_t)0x2902));
 
-
 #define smoke_gpio 15
 #define soil_gpio 2
+#define ONBOARD_LED 2
 int soilval;
 int smokeval;
 
-unsigned long lastTime = 0;
-unsigned long timerDelay = 2000;
-
 bool deviceConnected = false;
+bool oldDeviceConnected = false;
 int soil_sens = 0;
 int smoke_sens = 0;
 
-class myServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer){
+class myServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
     deviceConnected = true;
-    };
-  void onDisconnect(BLEServer* pServer){
-    deviceConnected = false;
-    };
   };
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+  };
+};
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
-  
-  //creating server
+  pinMode(ONBOARD_LED, OUTPUT);
+
+  // creating server
   BLEDevice::init("SensorNodeESP32");
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new myServerCallbacks());
-  
-  //creating service
+
+  // creating service
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  //creating 2 characteristics
+  // creating 2 characteristics
   pService->addCharacteristic(&soilCharacteristics);
   soilDescriptor.setValue("Soil Moisture");
   soilCharacteristics.addDescriptor(new BLE2902());
@@ -63,42 +66,63 @@ void setup() {
   smokeDescriptor.setValue("Smoke Density");
   smokeCharacteristics.addDescriptor(new BLE2902());
 
-
   pService->start();
   // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+
   pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
   pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
 }
 
-void loop() {
-  if (deviceConnected) {
-    if ((millis() - lastTime) > timerDelay) {
+void loop()
+{
+  analogReadResolution(10);
+  BLE();
+  delay(2000);
+}
 
-      analogReadResolution(10);
-      soilval = analogRead(soil_gpio);
-      smokeval = analogRead(smoke_gpio);
-      
-      static char soilvall[8];
-      dtostrf(soilval, 4, 2, soilvall);
-      static char smokevall[8];
-      dtostrf(smokeval, 4, 2, smokevall);
-      
-      
-      soilCharacteristics.setValue(soilvall);
-      soilCharacteristics.notify();   
-      Serial.print(" - Soil Moisture: ");
-      Serial.println(soilval);
+void BLE()
+{
+  if (deviceConnected)
+  {
 
-      
-      smokeCharacteristics.setValue(smokevall);
-      smokeCharacteristics.notify();   
-      Serial.print(" - Smoke Density: ");
-      Serial.println(smokeval);
-      
-      
-      lastTime = millis();
-    }
+    soilval = analogRead(soil_gpio);
+    smokeval = analogRead(smoke_gpio);
+
+    static char soilvall[8];
+    dtostrf(soilval, 4, 2, soilvall);
+    static char smokevall[8];
+    dtostrf(smokeval, 4, 2, smokevall);
+
+    soilCharacteristics.setValue(soilvall);
+    soilCharacteristics.notify();
+    Serial.print(" - Soil Moisture: ");
+    Serial.println(soilval);
+
+    smokeCharacteristics.setValue(smokevall);
+    smokeCharacteristics.notify();
+    Serial.print(" - Smoke Density: ");
+    Serial.println(smokeval);
+    Serial.print("Connection Status : ");
+    digitalWrite(ONBOARD_LED, HIGH);
+    delay(500);
+    // Serial.println(deviceConnected);
+  }
+  if (!deviceConnected && oldDeviceConnected)
+  {
+    delay(500);                  // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("Start Advertising");
+    Serial.println("Disconnecting");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected)
+  {
+    Serial.println("Connecting");
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
   }
 }
